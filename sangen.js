90697b7093fd23e4156c4da49e6e93a29410989a
Eric Mosher <emoshmosh@gmail.com>
2013-08-13 21:10:23 -0400

    Updating library structure and added wav support.

--- sangen.js
+++ sangen.js
@@ -5,42 +5,62 @@ var ffmpeg = require('fluent-ffmpeg');
  * Sangen constructor
  * 
  * Music conversion library.
- * @param {String} type the type of preffered conversion 
+ * @param {String} type the type of preffered conversion for use in shorthand 
  */
 var Sangen = function(type){
     this.type = type || 'mp3';
 };
 
 /**
- * Convert an audio file into a different type.
+ * Convert an audio file into a different type. Used as a shorthand method.
  * 
+ * @method convert
  * @param {String} fileName the file to convert
  * @param {String} targetName the desired name after conversion
  */
 Sangen.prototype.convert = function(fileName, targetName) {
-    if (this.type === 'mp3') {
-        this._toMp3(fileName, targetName);
+    switch(this.type) {
+    case 'mp3':
+        this.toMp3(fileName, targetName);
+        break;
+    case 'wav':
+        this.toWav(fileName, targetName);
+        break;
     }
-    
-    // TODO: implement more file types 
     
 };
 
 /**
  * Convert to mp3 format.
  *
- * @method _toMp3
- * @private
+ * @method toMp3
  * @param {String} fileName the file to convert
  * @param {String} targetName the desired name after conversion
  */
-Sangen.prototype._toMp3 = function(fileName, targetName) {
+Sangen.prototype.toMp3 = function(fileName, targetName) {
     var proc = new ffmpeg({ source: fileName })
         .withAudioBitrate('128k')
         .withAudioCodec('libmp3lame')
         .toFormat('mp3')
         .saveToFile(targetName, function(stdout, stderr) {
             console.log(fileName + ' has been converted to mp3 succesfully');
+        });
+};
+
+/**
+ * Convert to wav format.
+ *
+ * @method toWav
+ * @param {String} fileName the file to convert
+ * @param {String} targetName the desired name after conversion
+ */
+Sangen.prototype.toWav = function(fileName, targetName) {
+    var proc = new ffmpeg({ source: fileName })
+        .withAudioBitrate('128k')
+        .withAudioCodec('libmp3lame')
+        .toFormat('wav')
+        .saveToFile(targetName, function(stdout, stderr) {
+            console.log(fileName + ' has been converted to wav succesfully');
         });
 };
 
